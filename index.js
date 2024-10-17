b462827275d73a270797cee9a82300876a85241a
Rauniyar Badri Prasad (SDS/EDS) <bqu1kor@bosch.com>
2024-10-17 22:50:03 +0530

    starting simple function with the split string and adding the number without any other provisions

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function add(a, b) {
-    return a + b;
+function add(numberInString) {
+    console.log("numberInString",numberInString)
+    let arrayOfNumbers=numberInString.split(",")
+    console.log("arrayOfNumbers",arrayOfNumbers)
+    let sum=0
+    arrayOfNumbers.map(item=>{
+        console.log("item",item,typeof Number(item))
+        sum+=Number(item)
+    })
+    return sum;
 }
 
-console.log("The sum of 1 annd 2 is: " + add(1, 2));
-console.log("The sum of 10 and 20 is: " + add(10, 20));
+console.log("The sum of 1 annd 2 is: " + add("1,2"));
+// console.log("The sum of 10 and 20 is: " + add(""));
