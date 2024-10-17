478ec42715a4ae1a7544c054b4ee0ba99918c758
Rauniyar Badri Prasad (SDS/EDS) <bqu1kor@bosch.com>
2024-10-17 22:59:44 +0530

    handle of negative number

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function add(numberInString) {
-    console.log("numberInString",numberInString)
-    let arrayOfNumbers=numberInString.split(",")
-    console.log("arrayOfNumbers",arrayOfNumbers)
-    let sum=0
-    arrayOfNumbers.map(item=>{
-        console.log("item",item,typeof Number(item))
-        sum+=Number(item)
+    let negativeNumbers = []
+    let arrayOfNumbers = numberInString.split(",")
+    console.log("arrayOfNumbers", arrayOfNumbers)
+    let sum = 0
+    arrayOfNumbers.map(item => {
+        if (Number(item) >= 0) {
+            sum += Number(item)
+        }else{
+            negativeNumbers.push(Number(item))
+        }
     })
+    console.log("negativeNumbers",negativeNumbers)
+    if(negativeNumbers.length>0){
+        throw new Error(`Please enter only positive number , as negative number is not allowed",${negativeNumbers.join(",")}`)
+    }
     return sum;
 }
 
-console.log("The sum of 1 annd 2 is: " + add("1,2"));
-// console.log("The sum of 10 and 20 is: " + add(""));
+console.log("The sum of 10 and 20 is: " + add(""));
+console.log("The sum of 1 annd 2 is: " + add("1,2,5,6,6,7"));
+console.log("The sum of 1 annd 2 is: " + add("1,2,-5,6,-6,7"));
+
